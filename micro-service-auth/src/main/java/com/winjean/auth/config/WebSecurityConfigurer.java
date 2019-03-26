10c7171cb35934eda87018983423279851400400
winjean <19548901@qq.com>
2019-03-26 19:36:57 +0800

    add zuul route

---
+++ micro-service-auth/src/main/java/com/winjean/auth/config/WebSecurityConfigurer.java
@@ -0,0 +1,43 @@
+package com.winjean.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+/**
+ * @author ：winjean
+ * @date ：Created in 2019/3/26 19:20
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("guest").password("guest").authorities("WRIGTH_READ")
+                .and()
+                .withUser("admin").password("admin").authorities("WRIGTH_READ", "WRIGTH_WRITE");
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
