da87e97c8d781b9887b56608dec5398c82b0b2b9
winjean <19548901@qq.com>
2019-04-02 16:57:08 +0800

    add README.md

--- micro-service-config/src/main/java/com/winjean/config/ServiceConfigApplication.java
+++ micro-service-config/src/main/java/com/winjean/config/ServiceConfigApplication.java
@@ -1,14 +1,11 @@
-package com.winjean.auth;
+package com.winjean.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
-@SpringBootApplication
+@SpringCloudApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class ServiceConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceConfigApplication.class,args);
