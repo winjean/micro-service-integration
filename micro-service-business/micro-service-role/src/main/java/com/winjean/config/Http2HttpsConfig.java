cbed7b8b87c07bbeea8dde2eea31d96e8d784a0d
winjean <19548901@qq.com>
2019-04-09 16:34:32 +0800

    add README.md

--- micro-service-business/micro-service-role/src/main/java/com/winjean/config/Http2HttpsConfig.java
+++ micro-service-business/micro-service-role/src/main/java/com/winjean/config/Http2HttpsConfig.java
@@ -1,6 +1,9 @@
 package com.winjean.config;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -48,20 +51,20 @@ public class Http2HttpsConfig {
 
     @Bean
     public TomcatServletWebServerFactory tomcatServletWebServerFactory(){
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();{
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
 
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint constraint = new SecurityConstraint();
-////                INTEGRAL, or CONFIDENTIAL. tomcatapi用于配置访问时加密的，也就是支持https
-//                constraint.setUserConstraint("CONFIDENTIAL");
-//                //安全集合
-//                SecurityCollection collection = new SecurityCollection();
-//                //配置https的适配url
-//                collection.addPattern("/*");
-//                constraint.addCollection(collection);
-//                context.addConstraint(constraint);
-//            }
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+//                INTEGRAL, or CONFIDENTIAL. tomcatapi用于配置访问时加密的，也就是支持https
+                constraint.setUserConstraint("CONFIDENTIAL");
+                //安全集合
+                SecurityCollection collection = new SecurityCollection();
+                //配置https的适配url
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
         };
 
 		tomcat.addAdditionalTomcatConnectors(httpConnector());
@@ -74,10 +77,10 @@ public class Http2HttpsConfig {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         //connector监听的端口号
-        connector.setPort(8443);
+        connector.setPort(7201);
         connector.setSecure(false);
         //监听到http的端口后转向到https的端口
-        connector.setRedirectPort(7201);
+        connector.setRedirectPort(8443);
         return connector;
     }
 }
