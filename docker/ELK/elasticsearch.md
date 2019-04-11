3aca6da164175d3bca6ae1a29f0d9aea6f92c53e
winjean <19548901@qq.com>
2019-04-11 17:29:21 +0800

    add README.md

--- docker/ELK/elasticsearch.md
+++ docker/ELK/elasticsearch.md
@@ -1,8 +1,20 @@
 * 集群是否健康  
 curl -X GET http://192.168.71.128:9200/_cat/health?v
+
 * 查看集群节点数
 curl -X GET http://192.168.71.128:9200/_cat/nodes?v  
+
 * 查看集群全部索引
 curl -X GET http://192.168.71.128:9200/_cat/indices?v  
+
 * 创建索引
 curl -X PUT http://192.168.71.128:9200/customer?pretty
+
+* 删除索引
+curl -X DELETE http://localhost:9200/debug-role-2019.04.11
+
+* 创建文档
+curl -X PUT "localhost:9200/index/doc/1" -H 'Content-Type: application/json' -d'{"name": "winjean"}'
+
+* 查询文档
+curl -X GET "localhost:9200/index/doc/1"
--- micro-service-test/src/main/java/com/winjean/sample/controller/TestController.java
+++ micro-service-test/src/main/java/com/winjean/sample/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.winjean.sample.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.Random;
 
 @RestController
 @Slf4j
@@ -10,11 +14,38 @@ public class TestController {
     @GetMapping("/")
     @ResponseBody
     public String test() {
-        log.trace("--------------------------this is trace test !!--------------------------");
-        log.debug("--------------------------this is debug test !!--------------------------");
-        log.info("--------------------------this is info test !!--------------------------");
-        log.warn("--------------------------this is warn test !!--------------------------");
-        log.error("--------------------------this is error test !!--------------------------");
+        JSONObject json = new JSONObject();
+        json.put("datetime", new Date());
+        json.put("Random1", new Random().nextInt(10));
+        json.put("Random2", new Random().nextInt(10));
+        json.put("Random3", new Random().nextInt(10));
+        json.put("level","trace");
+        log.trace(json.toJSONString());
+
+        json.put("Random1", new Random().nextInt(10));
+        json.put("Random2", new Random().nextInt(10));
+        json.put("Random3", new Random().nextInt(10));
+        json.put("level","debug");
+        log.debug(json.toJSONString());
+
+        json.put("Random1", new Random().nextInt(10));
+        json.put("Random2", new Random().nextInt(10));
+        json.put("Random3", new Random().nextInt(10));
+        json.put("level","info");
+        log.info(json.toJSONString());
+
+        json.put("Random1", new Random().nextInt(10));
+        json.put("Random2", new Random().nextInt(10));
+        json.put("Random3", new Random().nextInt(10));
+        json.put("level","warn");
+        log.warn(json.toJSONString());
+
+        json.put("Random1", new Random().nextInt(10));
+        json.put("Random2", new Random().nextInt(10));
+        json.put("Random3", new Random().nextInt(10));
+        json.put("level","error");
+        log.error(json.toJSONString());
+
         return "-------------test index-------------";
     }
 }
