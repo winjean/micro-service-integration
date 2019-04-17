495c550b7a7fa605b4142144c28303f1791c07e2
winjean <19548901@qq.com>
2019-04-17 17:37:52 +0800

    add README.md

---
+++ micro-service-gateway/src/main/java/com/winjean/gateway/config/GatewayConfig.java
@@ -0,0 +1,21 @@
+package com.winjean.gateway.config;
+
+import com.winjean.gateway.filter.JwtCheckGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：winjean
+ * @date ：Created in 2019/4/17 17:33
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public JwtCheckGatewayFilterFactory jwtCheckGatewayFilterFactory(){
+        return new JwtCheckGatewayFilterFactory();
+    }
+}
