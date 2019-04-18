a0aeb56105030235f55a6f57e8d1d9a27795859d
winjean <19548901@qq.com>
2019-04-18 20:09:02 +0800

    add README.md

---
+++ docker/mongo/README.md
@@ -0,0 +1,7 @@
+## http://www.runoob.com/mongodb/mongodb-create-database.html
+* mongo
+* show dbs
+* db.winjean.insert({"winjean":"winjean"})
+* db.winjean.find({}).pretty()
+* db.winjean.update({"winjean":"winjean"},{"winjean":"winjean-1"})
+* db.winjean.remove({"winjean":"winjean-1"})
--- micro-service-business/micro-service-mongo/src/test/java/UserDaoTest.java
+++ micro-service-business/micro-service-mongo/src/test/java/UserDaoTest.java
@@ -1,8 +1,10 @@
+import com.winjean.mongo.MongoApplication;
 import com.winjean.mongo.model.entity.User;
 import com.winjean.mongo.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -16,6 +18,7 @@ import javax.annotation.Resource;
  */
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {MongoApplication.class})
 @SpringBootTest
 public class UserDaoTest {
 
