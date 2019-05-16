16951bd76b72b043c8fbaf216da18868d8413ca2
winjean <19548901@qq.com>
2019-05-16 09:59:50 +0800

    add README.md

--- micro-service-business/micro-service-base/src/main/java/com/winjean/exception/GlobalExceptionHandler.java
+++ micro-service-business/micro-service-base/src/main/java/com/winjean/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.winjean.exception;
 import com.winjean.common.BaseResponse;
 import com.winjean.common.exception.ExceptionUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.management.ReflectionException;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
@@ -28,94 +30,87 @@ import java.util.Set;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public Object globalExceptionHandler(Exception e) throws Exception {
+    public BaseResponse globalExceptionHandler(Exception e) throws Exception {
         log.error(ExceptionUtil.getTrace(e));
-        return e.getMessage();
+        return BaseResponse.getFailureResponse(e.getMessage());
     }
 
-    /*@ExceptionHandler(ReflectionException.class)
+    @ExceptionHandler(ReflectionException.class)
     public Object reflectionExceptionHandler(ReflectionException e) throws Exception {
         log.error(ExceptionUtil.getTrace(e));
         return BaseResponse.getFailureResponse("数据操作反射异常:" + e.getMessage());
-    }*/
+    }
 
     @ExceptionHandler(BindException.class)
-    public Object bindExceptionHandler(BindException e) {
+    public BaseResponse bindExceptionHandler(BindException e) {
         log.error(ExceptionUtil.getTrace(e));
         BindingResult result = e.getBindingResult();
         FieldError error = result.getFieldError();
         String field = error.getField();
         String code = error.getDefaultMessage();
         String message = String.format("%s:%s", field, code);
-        return "参数绑定失败="+message;
+        return BaseResponse.getFailureResponse("参数绑定失败="+message);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Object methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
-        log.error("参数绑定失败", e);
+    public BaseResponse methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        log.error(ExceptionUtil.getTrace(e));
+
+        MethodParameter parameter = e.getParameter();
+        String method = parameter.getMethod().getName();
+
         BindingResult result = e.getBindingResult();
         FieldError error = result.getFieldError();
         String field = error.getField();
         String code = error.getDefaultMessage();
-        String message = String.format("参数绑定失败: %s=%s", field, code);
+        String message = String.format("方法:%S, 参数绑定失败: %s=%s", method, field, code);
 
         return BaseResponse.getFailureResponse(message);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public String handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
-        return "缺少请求参数";
+    public BaseResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        log.error(ExceptionUtil.getTrace(e));
+        return BaseResponse.getFailureResponse("缺少请求参数");
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public String handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        return "参数解析失败:"+e.getMessage();
+    public BaseResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error(ExceptionUtil.getTrace(e));
+        return BaseResponse.getFailureResponse("参数解析失败:"+e.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public String handleServiceException(ConstraintViolationException e) {
+    public BaseResponse handleServiceException(ConstraintViolationException e) {
+        log.error(ExceptionUtil.getTrace(e));
         Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
         ConstraintViolation<?> violation = violations.iterator().next();
         String message = violation.getMessage();
-        return "参数验证失败" + message;
+        return BaseResponse.getFailureResponse("参数验证失败" + message);
     }
 
     @ExceptionHandler(ValidationException.class)
-    public String handleValidationException(ValidationException e) {
-        return "参数验证失败";
+    public BaseResponse handleValidationException(ValidationException e) {
+        log.error(ExceptionUtil.getTrace(e));
+        return BaseResponse.getFailureResponse("参数验证失败");
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public String noHandlerFoundException(NoHandlerFoundException e) {
-        return "Not Found="+e;
+    public BaseResponse noHandlerFoundException(NoHandlerFoundException e) {
+        log.error(ExceptionUtil.getTrace(e));
+        return BaseResponse.getFailureResponse("Not Found="+e.getMessage());
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public Object handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+    public BaseResponse handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         log.error(ExceptionUtil.getTrace(e));
-        e.printStackTrace();
         return BaseResponse.getFailureResponse("request_method_not_supported:"+e.getMethod());
     }
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public String handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
-        return "content_type_not_supported";
+    public BaseResponse handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        log.error(ExceptionUtil.getTrace(e));
+        return BaseResponse.getFailureResponse("content_type_not_supported");
     }
 
-//    @ExceptionHandler(NoHandlerFoundException.class)
-//    public String noHandlerFoundExceptionHandler(NoHandlerFoundException e) {
-//        return "NoHandlerFoundException : "+e.getMessage();
-//    }
-
-    /*@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(ServiceException.class)
-    public String handleServiceException(ServiceException e) {
-        return "业务逻辑异常：" + e.getMessage();
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(DaoException.class)
-    public String handleException(DaoException e) {
-        return "操作数据库出现异常：字段重复、有外键关联等";
-    }*/
 }
