2aa5b2d541ecb357a8036b673e9669b9dbbec356
winjean <wangjian@chinaliancheng.com>
2022-04-27 17:09:22 +0800

    【提交类型】: 新功能 【影响范围】: 【提交简述】: 【详细描述】: 修改spring could版本 2021.0.1 【不兼容变动】: 【相关单号】: 【需要测试】: 否

---
+++ micro-service-nacos-discovery/src/main/java/com/winjean/nacos/discovery/NacosDiscoveryApplication.java
@@ -0,0 +1,7 @@
+package com.winjean.nacos.discovery;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+public class NacosDiscoveryApplication {
+}
